1296134ee4e4926d9a97bf174c31d36375a2fe2e
Wong Fei Zhu <wongfei.zhu@gmail.com>
2015-11-27 22:45:38 -0800

    Moved all the user prompts into a method "fprompts"

--- apflooder.sh
+++ apflooder.sh
@@ -7,48 +7,60 @@ BLU=$(echo -e "\e[1;36m")
 GRN=$(echo -e "\e[1;32m")
 RST=$(echo -e "\e[0;0;0m")
 
+
+fprompts(){
+	clear 
+	printf $GRN"\nAccess Point Spammer - Flood Area with Fake Access Points\n"$RST
+	printf $GRN"To get started, let's get some information first"$RST
+	
+	
+	#read wireless card name
+	printf $BLU"\nEnter your Network Interface Card: "$RST
+	read CARD_NAME
+	
+	#ask for mangle or not
+	printf $BLU"\nWould you like to use the mangler (y/n)? \nEnter (y) for mangler and (n) for ESSID file: "$RST
+	read MANGLER 
+	
+	#branch on mangler or not
+	if [ $MANGLER = "y" ]; then
+		printf $BLU"Enter the word you would like to mangle : "$RST
+		read MANGLE_WORD
+	else
+		#read access point file name/location
+		printf $BLU"Enter the path to you ESSID file: "$RST
+		read ESSID_FILE
+		if [ ! -e $ESSID_FILE ]; then
+			printf $RED"[-] File does not exist.. exiting\n"$RST
+			exit
+		else
+			NUM_OF_ESSIDS=$(wc -l < $ESSID_FILE)
+		fi
+	fi 
+}
+
 fbssid(){
+	printf "Generating BSSIDs ... Please wait.."
 	LIST=()
 	#Generate Random MAC Addresses For Our Access Points
 	for ((i=0;i<$NUM_OF_ESSIDS;i++))
 	do
 		LIST[$i]=$(openssl rand -hex 6 | sed 's/\(..\)/\1:/g; s/.$//')
 	done
+	
+	#Temp debug - print out bssids generated
+	for i in "${LIST[@]}"
+	do
+		printf $i"\n"
+	done
 		
 }
 
-clear 
-printf $GRN"\nAccess Point Spammer - Flood Area with Fake Access Points\n"$RST
-printf $GRN"To get started, let's get some information first"$RST
+#Prompt for user inputs
+fprompts 
 
-
-#read wireless card name
-printf $BLU"\nEnter your Network Interface Card: "$RST
-read CARD_NAME
-
-#ask for mangle or not
-printf $BLU"\nWould you like to use the mangler (y/n)? \nEnter (y) for mangler and (n) for ESSID file: "$RST
-read MANGLER 
-
-#branch on mangler or not
-if [ $MANGLER = "y" ]; then
-	printf $BLU"Enter the word you would like to mangle : "$RST
-	read MANGLE_WORD
-else
-	#read access point file name/location
-	printf $BLU"Enter the path to you ESSID file: "$RST
-	read ESSID_FILE
-	if [ ! -e $ESSID_FILE ]; then
-		printf $RED"[-] File does not exist.. exiting\n"$RST
-		exit
-	else
-		NUM_OF_ESSIDS=$(wc -l < $ESSID_FILE)
-		
-	fi
-fi 
-
-#continue our processing, seems like all is ok
-
+#lets continue doing what we are doing, seems all is ok
+fbssid $NUM_OF_ESSIDS
 
 printf $GRN"\nThe NIC is "$RST$BLU"$CARD_NAME"$RST
 printf $GRN"\nThe essid file location is "$RST$BLU"$ESSID_FILE"$RST
